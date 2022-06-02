77eb4eb730a235551749fb44362dec186fa410ad
kasAndrey <75616570+kasAndrey@users.noreply.github.com>
2022-06-02 20:06:52 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "FB.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    AdjMatrix graph;
+    ifstream fin;
+    const char filename[] = "input.txt";
+
+    fin.open(filename);
+    if (fin.good()) fin >> graph;
+    else return 5;
+
+    cout << "Initial graph's adjecent matrix is\n" << graph << "\n";
+    cout << "After applying Bellman-Ford algorithm:\n" << graph.FordBellman() << endl;
+
+    fin.close();
+    return 0;
+}
